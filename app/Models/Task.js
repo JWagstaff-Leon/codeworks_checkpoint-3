b313ad4f7c39734a3795af5976a1eb762dfd2431
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-10 21:35:42 -0600

    updated icon and removed drop shadow

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -22,7 +22,7 @@ export class Task
 
     get Template()
     {
-        const iconClass = this.checked ? "mdi mdi-24px mdi-checkbox-marked-circle" : "mdi mdi-24px mdi-circle-outline";
+        const iconClass = this.checked ? "mdi mdi-24px mdi-checkbox-marked-circle-outline" : "mdi mdi-24px mdi-circle-outline";
         const textStyle = this.checked ? "text-decoration: line-through; " : ""
         
         const parentList = ProxyState.lists.find(list => list.id === this.listId);
@@ -37,7 +37,7 @@ export class Task
         return `
             <div class="d-flex align-items-center">
                 <div onclick="app.listsController.toggleTask('${this.id}')" class="d-flex align-items-center flex-grow-1 selectable no-select">
-                    <i class="${iconClass} me-2" style="${iconStyle} filter: drop-shadow(4px 4px 0.8px rgba(0, 0, 0, 1));"></i>
+                    <i class="${iconClass} me-2" style="${iconStyle}"></i>
                     <span style="${textStyle}">${this.name}</span>
                 </div>
                 <i class="mdi mdi-24px action mdi-delete-forever on-hover text-danger ms-2" onclick="app.listsController.deleteTask('${this.id}')"></i>
